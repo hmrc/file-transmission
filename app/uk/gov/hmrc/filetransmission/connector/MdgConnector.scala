32c4e223e473a3b615f967e663f9fd9700067f5f
Tomasz Rosiek <tomasz.rosiek@digital.hmrc.gov.uk>
2018-08-15 16:34:45 +0100

    PLATY-651 Fail fast when bad request returned

--- app/uk/gov/hmrc/filetransmission/connector/MdgConnector.scala
+++ app/uk/gov/hmrc/filetransmission/connector/MdgConnector.scala
@@ -20,25 +20,38 @@ import javax.inject.Inject
 import play.api.http.{ContentTypes, HeaderNames}
 import uk.gov.hmrc.filetransmission.config.ServiceConfiguration
 import uk.gov.hmrc.filetransmission.model.TransmissionRequest
-import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.http.{BadRequestException, HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import cats.implicits._
 
 import scala.concurrent.{ExecutionContext, Future}
+
+sealed trait MdgRequestResult
+case object MdgRequestSuccessful extends MdgRequestResult
+case class MdgRequestError(e: Throwable) extends MdgRequestResult
+case class MdgRequestFatalError(e: Throwable) extends MdgRequestResult
 
 class MdgConnector @Inject()(
   httpClient: HttpClient,
   serviceConfiguration: ServiceConfiguration,
   requestSerializer: MdgRequestSerializer)(implicit ec: ExecutionContext) {
 
-  def requestTransmission(request: TransmissionRequest)(implicit hc: HeaderCarrier): Future[Unit] = {
+  def requestTransmission(request: TransmissionRequest)(implicit hc: HeaderCarrier): Future[MdgRequestResult] = {
 
     val serializedRequest: String = requestSerializer.serialize(request)
-    httpClient
-      .POSTString[HttpResponse](
-        serviceConfiguration.mdgEndpoint,
-        serializedRequest,
-        Seq((HeaderNames.CONTENT_TYPE, ContentTypes.XML)))
-      .map(_ => ())
+    for (result <- httpClient
+                    .POSTString[HttpResponse](
+                      serviceConfiguration.mdgEndpoint,
+                      serializedRequest,
+                      Seq((HeaderNames.CONTENT_TYPE, ContentTypes.XML)))
+                    .attempt) yield {
+      result match {
+        case Right(_)                     => MdgRequestSuccessful
+        case Left(e: BadRequestException) => MdgRequestFatalError(e)
+        case Left(e)                      => MdgRequestError(e)
+      }
+    }
+
   }
 
 }
