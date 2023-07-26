e9bf9fa5573bd4dbd0dd9e72d054da8dc1935374
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2023-07-26 10:18:33 +0100

    Update bootstrap-play

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,20 +4,20 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "5.24.0"
-  val hmrcMongoVersion = "0.73.0"
+  val bootstrapVersion = "7.20.0"
+  val hmrcMongoVersion = "1.3.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"         % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"                % hmrcMongoVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-work-item-repo-play-28" % hmrcMongoVersion,
-    "org.typelevel"           %% "cats-core"                         % "2.6.1",
+    "org.typelevel"           %% "cats-core"                         % "2.9.0"
 
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion   % "test, it",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % hmrcMongoVersion   % Test,
-    "org.mockito"             %% "mockito-scala-scalatest"    % "1.17.5"           % Test,
+    "org.mockito"             %% "mockito-scala-scalatest"    % "1.17.5"           % Test
   )
 }
