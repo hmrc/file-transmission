ade4c48c9aebbcfcaf8738c834632982aba27093
victorarbuesmallada <victor.arbues@digital.hmrc.gov.uk>
2023-11-24 13:34:52 +0000

    BDOG-2800: Update to Play 3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,20 +4,20 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.20.0"
-  val hmrcMongoVersion = "1.3.0"
+  val bootstrapVersion = "8.0.0"
+  val hmrcMongoVersion = "1.5.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"         % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"                % hmrcMongoVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-work-item-repo-play-28" % hmrcMongoVersion,
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"         % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                % hmrcMongoVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-work-item-repo-play-30" % hmrcMongoVersion,
     "org.typelevel"           %% "cats-core"                         % "2.9.0"
 
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion   % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % hmrcMongoVersion   % Test,
-    "org.mockito"             %% "mockito-scala-scalatest"    % "1.17.5"           % Test
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion   % Test,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion   % Test,
+    "org.mockito"             %% "mockito-scala-scalatest"    % "1.17.29"          % Test
   )
 }
