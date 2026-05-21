3ff3ffe0f0b014c0ddfdb1cf3fb6b17fd9eb2487
Thomas Sarno <Thomassarno@hotmail.com>
2026-05-21 14:41:43 +0100

    DCWL-4511, remediate vulnerabilities (#72)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.16.0"
-  val hmrcMongoVersion = "2.6.0"
+  val bootstrapVersion = "10.7.0"
+  val hmrcMongoVersion = "2.12.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"         % bootstrapVersion,
